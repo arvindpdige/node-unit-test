55910698d014e8590fe6b3113e1bef61d5938c3b
arvindpdige <arvindpdige@gmail.com>
2025-11-21 18:42:06 +0530

    test cc

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,21 @@
-const { add } = require("../src/math.js");
+const { add, subtract, multiply, divide } = require("../src/math");
 
-describe("math", () => {
-  test("adds two numbers", () => {
-    expect(add(2, 3)).toBe(5);
-  });
+test("add works", () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test("subtract works", () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test("multiply works", () => {
+  expect(multiply(4, 3)).toBe(12);
+});
+
+test("divide works", () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test("divide throws on zero", () => {
+  expect(() => divide(5, 0)).toThrow("Cannot divide by 0");
 });
